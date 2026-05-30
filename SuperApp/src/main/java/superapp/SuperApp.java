556680ef71e1bd66979aa81f6a2a39b7ff7f9e6e
reigenecsxist <gabrielbiel.mesquita7@gmail.com>
2026-05-30 18:13:03 -0300

    Primeiro commit.

---
+++ SuperApp/src/main/java/superapp/SuperApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package superapp;
+
+/**
+ *
+ * @author Usuário do Windows
+ */
+public class SuperApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
